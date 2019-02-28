467482c19e03192d5a4ca29d97f1501664fbc822
DigitaMind <abdullahi.monsur.olalekan@gmail.com>
2019-02-28 10:07:48 +0100

    Initialize Project

---
+++ src/test/java/com/interswitch/messagingkafka/MessagingKafkaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.interswitch.messagingkafka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MessagingKafkaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
